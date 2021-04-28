7a702da3e2c3cc0876007d619075dc68b5bc91ad
Glitch (nostalgic-aboard-bedbug) <none>
2021-04-28 09:59:53 +0000

    💕🕰 Checkpoint ./komutlar/yardım.js:25419324/3186

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,34 +1,47 @@
-const Discord = require('discord.js');
+const Discord = require ("discord.js");
 
-exports.run = (client, message, args) => {
- const embed = new Discord.MessageEmbed()
- .setAuthor(`${client.user.username} Yardım Menüsü`,client.user.avatarURL())
- .setThumbnail(message.author.avatarURL())
- .setColor('RANDOM')
- .setDescription(`
- > \`!antiraid -> Sunucuya izinsiz bot giremez.\`
+exports.run = (client, message) => {
+const lembed = new Discord.MessageEmbed()
 
- > \`!antiraidbotizni -> Botun sunucuya girmesi için izin verirsiniz.\` 
+.then;   
+const mhelp = new Discord.MessageEmbed()
+.setColor("RANDOM")
+.setAuthor(`${client.user.username} `, client.user.avatarURL)  
+.setTitle(" Sharpen Bot | Yardım Menüsü")
+.setThumbnail("https://cdn.discordapp.com/emojis/727894683061321759.gif?v=1")
+    .setDescription(`
 
- > \`!ban -> Üyeyi banlarsın.\`
- 
- > \`!ban -> Üyeyi banlarsın.\`
- > \`s!davet -> Botun Davetini ve Destek Sunucunu gösterir.\``)
- .setFooter(`Sharpen Sunar .`)
-    .setTimestamp()
-message.channel.send(embed) 
-//sharpen
-};
 
+╔════════════════════════════════════╗
+║**»** **!ban** : Sunucudan bir üyeyi yasaklar.
+║**»** **!kick** : Sunucudan bir üyeyi atar.
+║**»** **!sil** : Belirli bir kanaldaki mesajları siler.
+║**»** **!antiraid** : Sunucuya izinsiz bot girmez.
+║**»** **!antiraidbotizni** : Sunucuya izinsiz bot girmez.
+║**»** **!yavaş-mod** : Sohbete yazma sınır (süre) ekler.
+║**»** **!sunucu-kur** : Bot sunucu kurar.
+║**»** **!küfür-engel** : Küfür engel açıp kapatırsın.
+║**»** **!reklam-engelle** : Reklam engel açıp kapatırsın.
+║**»** **!nuke** : Kanala nuke atarsın.
+║
+║
+╚════════════════════════════════════╝
+`) 
+       .addField(`»  Bot Bağlantıları`, `  [Bot Davet Linki](https://discord.com/api/oauth2/authorize?client_id=728920362636542032&permissions=8&scope=bot) **|** [Destek Sunucusu](https://discord.gg/G5CZFT3) **|** [WebSitesi](http://ab.arrow-bot.tk/) :ate:`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz 
+        .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
+message.channel.send(mhelp)
+.then; const sembed = new Discord.MessageEmbed()
+
+}; 
 exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'yardım',   //sharpen
-  description: '',
-  usage: ''
-};
+    enabled: true, 
+    guildOnly: false, 
+    aliases: ["mod"], 
+    permLevel: 0 
+  };
+ 
+  exports.help = {
+    name: 'yardım', 
+    description: 'yardım menüsü',
+    usage: 'yardım'
+  };
