5062416cbc942d221815bbdddf84674b5aaf48d4
Glitch (nostalgic-aboard-bedbug) <none>
2021-05-29 10:01:28 +0000

    🎍🚔 Checkpoint ./readme.MD:20882587/1522 ./package.json:20882587/36 ./events/ready.js:20882587/267

--- events/ready.js
+++ events/ready.js
@@ -11,5 +11,4 @@ module.exports = client => {
   client.user.setStatus("dnd");
   client.user.setActivity("youtube.com/GweepCreativeOfficial", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
